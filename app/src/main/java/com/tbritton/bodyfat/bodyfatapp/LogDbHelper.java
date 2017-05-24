8916a21bef0dfb65d8f8d258d119d66bb8656066
PRDeltoid <tbrit10@gmail.com>
2017-05-23 18:38:53 -0700

    Upgraded and debugged Log button functionality
    Log button now displays a success/failure message and returns the user to the home screen

--- app/src/main/java/com/tbritton/bodyfat/bodyfatapp/LogDbHelper.java
+++ app/src/main/java/com/tbritton/bodyfat/bodyfatapp/LogDbHelper.java
@@ -2,8 +2,10 @@ package com.tbritton.bodyfat.bodyfatapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 public class LogDbHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
@@ -26,13 +28,14 @@ public class LogDbHelper extends SQLiteOpenHelper {
     //database helper singleton
     private static LogDbHelper mDbHelper = null;
 
-
     public LogDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        mDbHelper = new LogDbHelper(context);
     }
 
-    static void log(Context application_context, LogEntry log_entry) {
+    static void log(Context context, LogEntry log_entry) {
+        if (mDbHelper == null) {
+            mDbHelper = new LogDbHelper(context);
+        }
         //Open our database
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
@@ -49,7 +52,15 @@ public class LogDbHelper extends SQLiteOpenHelper {
         values.put(LogContract.LogEntry.COLUMN_NAME_DATETIME, log_entry.get_date());
 
         //Insert information into the database
-        db.insert(LogContract.LogEntry.TABLE_NAME, null, values);
+        try {
+            long record_id = db.insertOrThrow(LogContract.LogEntry.TABLE_NAME, null, values);
+        } catch (SQLException sql_exception) {
+            //Failed to insert. Show error message and exit method
+            Toast.makeText(context, "Failed to Log", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Successfully logged the bodyweight
+        Toast.makeText(context, "Logged.", Toast.LENGTH_SHORT).show();
     }
 
     public void onCreate(SQLiteDatabase db) {
--- app/src/main/java/com/tbritton/bodyfat/bodyfatapp/ThreeMeasure.java
+++ app/src/main/java/com/tbritton/bodyfat/bodyfatapp/ThreeMeasure.java
@@ -1,51 +1,66 @@
 package com.tbritton.bodyfat.bodyfatapp;
 
-import android.content.ContentValues;
-import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
-import android.preference.PreferenceManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static java.text.DateFormat.getDateTimeInstance;
-
-
-
 public class ThreeMeasure extends AppCompatActivity {
+    EditText measure_one_text,
+             measure_two_text,
+             measure_three_text;
+    Button   log_button;
+    final int MEASURE_TYPE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three_measure);
+
+        //Assign components to variables
+        measure_one_text   = (EditText) findViewById(R.id.measure_1_text);
+        measure_two_text   = (EditText) findViewById(R.id.measure_2_text);
+        measure_three_text = (EditText) findViewById(R.id.measure_3_text);
+        log_button = (Button) findViewById(R.id.three_measure_log_button);
+
+        //Create an action for the log button
+        log_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                log(v);
+                Intent main_activity_intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(main_activity_intent);
+            }
+        });
     }
 
     public void log(View view) {
         //Get values from fields and convert them to integers using parseInt
-        EditText measure_one_text   = (EditText) findViewById(R.id.measure_1_text);
-        EditText measure_two_text   = (EditText) findViewById(R.id.measure_2_text);
-        EditText measure_three_text = (EditText) findViewById(R.id.measure_3_text);
         int measure_one   = Integer.parseInt(measure_one_text.getText().toString());
         int measure_two   = Integer.parseInt(measure_two_text.getText().toString());
         int measure_three = Integer.parseInt(measure_three_text.getText().toString());
 
         //Calculate sum
         int sum = measure_one + measure_two + measure_three;
-        int age = 25; //Temporary hardcode
-        double weight = 200.0; //Temporary hardcode
-        int measure_type = 3;
+
+        //Temporary hardcoded variables
+        //These will be moved to Settings later
+        int age = 25;
+        double weight = 200.0;
+
         //Generate a date for the log
-        DateFormat dateFormatter = new SimpleDateFormat("YYYY-MM-DD HH:MM");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd hh:mm");
         dateFormatter.setLenient(false);
         String date = dateFormatter.format(new Date());
 
         //Create our log entry to give to our logger
-        LogEntry log_entry = new LogEntry(age, sum, measure_type, "Male", weight, date);
+        LogEntry log_entry = new LogEntry(age, sum, MEASURE_TYPE, "Male", weight, date);
         LogDbHelper.log(getApplicationContext(), log_entry);
     }
 }
