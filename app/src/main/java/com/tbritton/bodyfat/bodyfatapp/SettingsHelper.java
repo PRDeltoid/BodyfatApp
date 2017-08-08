c1b1827e5e5e4291e1c7c6d253bb3f4e6c4049b7
PRDeltoid <PRDeltoid@users.noreply.github.com>
2017-08-08 16:07:37 -0700

    Add SettingsHelper to pull settings information
    Added a placeholder class which will return current setting options, formatting, etc.
    
    Currently only returns static values for dev purposes

---
+++ app/src/main/java/com/tbritton/bodyfat/bodyfatapp/SettingsHelper.java
@@ -0,0 +1,13 @@
+package com.tbritton.bodyfat.bodyfatapp;
+
+
+public class SettingsHelper {
+    //Temporary placeholders until Settings are implemented
+    public static int get_age() {
+        return 25;
+    }
+
+    public static String get_sex() {
+        return "Male";
+    }
+}
